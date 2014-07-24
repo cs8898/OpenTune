4f5c407dd5d9aa7b56d5ff421fe5913679a5ac7b
cs8898 <cs8898@gmx.de>
2014-07-24 16:09:01 +0200

    i dont know
    try to manage slider.js

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,3 +1,7 @@
 function onSlide(obj){
-	$(obj).attr("id");
+	var instr_id = parseInt($(obj).attr("id").replace("instr_","").replace("_slider",""));
+	var slider_val = $(obj).val();
+	var abs_val = instrumente[instr_id].starttuning+slider_val;
+	instrumente[instr_id].tuning = abs_val;
+	alert(abs_val);
 }
