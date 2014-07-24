8e2cd34e434ce7b0e06493edc57c258bedce31f7
cs8898 <cs8898@gmx.de>
2014-07-24 12:08:16 +0200

    Edit generate_konkreteElemente fix position System

--- src/js/generate_konkreteElemente.js
+++ src/js/generate_konkreteElemente.js
@@ -1,7 +1,51 @@
-var VarInstrumentListe = $("#instrumentVorlagen div");
+var instrument_frame_width = 90;
+var instrument_frame_height = 80;
 
-for ( var i = 0; i < VarInstrumentListe.length; i++){
-	name = $(VarInstrumentListe[i]).find("span").text();
-	count = parseInt($(VarInstrumentListe[i]).find($("input[type='number']")).val());
-	alert(name+"|"+count);
+function generate_konkreteElemente(){
+	var VarInstrumentListe = $("#instrumentVorlagen div");
+	var instrcounter = 0;
+	
+	for ( var i = 0; i < VarInstrumentListe.length; i++){
+		type = $(VarInstrumentListe[i]).attr("id").replace("vorlage_",""); 
+		name = $(VarInstrumentListe[i]).find("span").text();
+		count = parseInt($(VarInstrumentListe[i]).find($("input[type='number']")).val());
+		//alert(type+"|"+name+"|"+count);
+		
+		for (var a = 1; a <= count; i++){
+			instrumente.push({
+				"id": "instr_"+instrID,
+				"type": type,
+				"tuning": 0,
+				"starttuning": 0,
+				"x": getNextInstrumentPosByID("x",instrID),
+				"y": getNextInstrumentPosByID("y",instrID)
+			});
+			instrID++;
+		}
+	}
+}
+
+function getNextInstrumentPosByID(aus,instrID){
+	var maxHor = Math.floor($(".SpielFlaeche").width()/instrument_frame_width);
+	var HorGap = ($(".SpielFlaeche").width()-(maxHor * instrument_frame_width))/(maxHor+1);
+	var maxVer = Math.floor($(".SpielFlaeche").height()/instrument_frame_height);
+	var VerGap = ($(".SpielFlaeche").height()-(maxVer * instrument_frame_height))/(maxVer+1);
+	
+	if (instrID <= maxHor){
+		xN = instrID;
+		yN = 0;
+	} else {
+		xN = maxHor%instrID;
+		yN = (instrID-yN)/maxHor;
+	}
+	
+	x = HorGap + (HorGap + (instrument_frame_width+HorGap) * ( xN - 1 ) );
+	y = VerGap + (VerGap + (instrument_frame_height+HorGap) * (yN - 1 ) );
+	if(aus == "x"){
+		return x;
+	} else if(aus == "y"){
+		return y;
+	} else {
+		return [x,y];
+	}
 }
--- src/js/gui.js
+++ src/js/gui.js
@@ -7,7 +7,7 @@ function befuelleInstrumentenVorlageListe() {
 		var insName = instrumentVorlagen[i].name;
 		var insMax = instrumentVorlagen[i].maxAnzahl;
 		var insAnzahl = instrumentVorlagen[i].anzahl;
-		var insId = "vorlage_" + instrumentVorlagen[i].type; //z.B. "anzahl_baritone"
+		var insId = "vorlage_" + instrumentVorlagen[i].type; //z.B. "vorlage_baritone"
 		var s = '<div id="' + insId + '"><span>' + insName + '</span> <input class="checkboxC" type="number" min="0" max="' + insMax + '" value="' + insAnzahl + '"/></div><br />';
 		$(liste).append(s);
 	}
